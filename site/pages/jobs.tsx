bd3e9c299f6d38008d12b7b45d28315d5665f65c
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-23 17:55:13 +0200

    feat(site): add placeholders for main pages

---
+++ site/pages/jobs.tsx
@@ -0,0 +1,30 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import React from 'react';
+
+import {
+  MainLayoutData,
+  MainLayoutContainer,
+  getMainLayoutData,
+} from './layout';
+
+interface Props extends MainLayoutData {}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const layoutData = await getMainLayoutData();
+  return { props: layoutData };
+};
+
+export default function JobsPage({ headerMenu }: Props): React.ReactElement {
+  return (
+    <>
+      <Head>
+        <title>Foursevens Jobs</title>
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
+      <MainLayoutContainer headerMenu={headerMenu}>
+        <h1>About</h1>
+      </MainLayoutContainer>
+    </>
+  );
+}
