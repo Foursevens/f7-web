62b033701760c80dde7d233e24ac839c9ab2b040
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-30 18:55:49 +0200

    feat(design): add content block

--- design/components/index.ts
+++ design/components/index.ts
@@ -2,6 +2,7 @@ export * from './stitches.config';
 
 export * from './button-link';
 export * from './container';
+export * from './content-block';
 export * from './header';
 export * from './menu';
 export * from './paragraph';
