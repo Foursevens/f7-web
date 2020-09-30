24523dde9a5d96630e07fad7261aed2f75d90aa3
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-30 18:55:49 +0200

    feat(design): add content block

---
+++ design/components/content-block.stories.tsx
@@ -0,0 +1,48 @@
+import { Story } from '@storybook/react/types-6-0';
+import React from 'react';
+
+import image from './assets/duurzaamheid.png';
+import { ContentBlock, ContentBlockProps } from './content-block';
+import { LOREM } from './data';
+
+export default {
+  title: 'Example/ContentBlock',
+  component: ContentBlock,
+};
+
+const Template: Story<ContentBlockProps> = (args): React.ReactElement => (
+  <ContentBlock {...args} />
+);
+
+export const WithImageAtStart = Template.bind({});
+WithImageAtStart.args = {
+  content: LOREM,
+  image,
+  imagePosition: 'start',
+  title: 'Lorem Ipsum',
+};
+
+export const WithImageAtEnd = Template.bind({});
+WithImageAtEnd.args = {
+  content: LOREM,
+  image,
+  imagePosition: 'end',
+  title: 'Lorem Ipsum',
+};
+
+export const WithCta = Template.bind({});
+WithCta.args = {
+  content: LOREM,
+  ctaChildren: 'Go on',
+  ctaHref: '/',
+  image,
+  title: 'Lorem Ipsum',
+};
+
+export const WithTag = Template.bind({});
+WithTag.args = {
+  content: LOREM,
+  image,
+  tag: 'Lorem Ipsum is simply dummy text',
+  title: 'Lorem Ipsum',
+};
---
+++ design/components/content-block.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import { ButtonLink } from './button-link';
+import { Paragraph } from './paragraph';
+import { styled } from './stitches.config';
+import { Tag } from './tag';
+
+const StyledBlock = styled('section', {
+  display: 'flex',
+  justifyItems: 'space-around',
+
+  '> *': {
+    maxWidth: '50%',
+  },
+
+  variants: {
+    imagePosition: {
+      start: {
+        '> *:nth-child(1)': { order: 1 },
+        '> *:nth-child(2)': { order: 2, paddingLeft: 96 },
+      },
+      end: {
+        '> *:nth-child(1)': { order: 2 },
+        '> *:nth-child(2)': { order: 1, paddingRight: 96 },
+      },
+    },
+  },
+});
+
+const StyledContent = styled('div', {
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  justifyContent: 'center',
+
+  header: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    marginBottom: '$large',
+
+    '* + *': {
+      marginTop: '$small',
+    },
+  },
+  'main p': {
+    marginBottom: '$medium',
+  },
+});
+
+interface PropsBase {
+  content: string;
+  ctaChildren?: React.ReactNode;
+  ctaHref?: string;
+  tag?: string;
+  title: string;
+  image: string;
+  imagePosition: 'start' | 'end';
+}
+
+export type ContentBlockProps =
+  | (PropsBase & { ctaHref: undefined; ctaChildren: undefined })
+  | (PropsBase & { ctaHref: string; ctaChildren: React.ReactNode });
+
+export function ContentBlock({
+  content,
+  ctaChildren,
+  ctaHref,
+  image,
+  imagePosition = 'start',
+  tag,
+  title,
+}: ContentBlockProps): React.ReactElement {
+  return (
+    <StyledBlock imagePosition={imagePosition}>
+      <div>
+        <img alt="decorative" aria-hidden="true" src={image} />
+      </div>
+      <StyledContent>
+        <header>
+          <h1>{title}</h1>
+          <Tag>{tag}</Tag>
+        </header>
+        <main>
+          <Paragraph>{content}</Paragraph>
+          {ctaHref == null ? null : (
+            <ButtonLink href={ctaHref}>{ctaChildren}</ButtonLink>
+          )}
+        </main>
+      </StyledContent>
+    </StyledBlock>
+  );
+}
