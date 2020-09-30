95c991f5aa97680d09f2ca1cc88e34fba85719b6
Dieter Luypaert <dieterluypaert@gmail.com>
2020-09-30 12:33:11 +0200

    design(menu): add active states

--- design/components/menu.stories.tsx
+++ design/components/menu.stories.tsx
@@ -12,17 +12,8 @@ const Template: Story<MenuProps> = (args): React.ReactElement => (
   <Menu {...args} />
 );
 
-export const Single = Template.bind({});
-Single.args = {
-  children: (
-    <MenuItem>
-      <a href="/">Home</a>
-    </MenuItem>
-  ),
-};
-
-export const Multiple = Template.bind({});
-Multiple.args = {
+export const Default = Template.bind({});
+Default.args = {
   children: [
     <MenuItem key="1">
       <a href="/">Home</a>
@@ -36,8 +27,23 @@ Multiple.args = {
   ],
 };
 
-export const MultipleWithHighlight = Template.bind({});
-MultipleWithHighlight.args = {
+export const WithActive = Template.bind({});
+WithActive.args = {
+  children: [
+    <MenuItem key="1" active="yes">
+      <a href="/">Home</a>
+    </MenuItem>,
+    <MenuItem key="2">
+      <a href="/">Cases</a>
+    </MenuItem>,
+    <MenuItem key="3">
+      <a href="/">Stories</a>
+    </MenuItem>,
+  ],
+};
+
+export const WithHighlight = Template.bind({});
+WithHighlight.args = {
   children: [
     <MenuItem key="1">
       <a href="/">Home</a>
@@ -48,7 +54,25 @@ MultipleWithHighlight.args = {
     <MenuItem key="3">
       <a href="/">Stories</a>
     </MenuItem>,
-    <MenuItem key="4" highlight>
+    <MenuItem key="4" highlight="yes">
+      <a href="/">Contact</a>
+    </MenuItem>,
+  ],
+};
+
+export const WithActiveHighlight = Template.bind({});
+WithActiveHighlight.args = {
+  children: [
+    <MenuItem key="1">
+      <a href="/">Home</a>
+    </MenuItem>,
+    <MenuItem key="2">
+      <a href="/">Cases</a>
+    </MenuItem>,
+    <MenuItem key="3">
+      <a href="/">Stories</a>
+    </MenuItem>,
+    <MenuItem key="4" active="yes" highlight="yes">
       <a href="/">Contact</a>
     </MenuItem>,
   ],
