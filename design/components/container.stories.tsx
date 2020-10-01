681401bb43b9ad1198e6972fcd1a1009b7f83aad
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-01 10:54:36 +0200

    feat(design): improve containers

--- design/components/container.stories.tsx
+++ design/components/container.stories.tsx
@@ -2,6 +2,8 @@ import { Story } from '@storybook/react/types-6-0';
 import React from 'react';
 
 import { Container, ContainerProps } from './container';
+import { LOREM } from './data';
+import { Paragraph } from './paragraph';
 
 export default {
   title: 'Example/Container',
@@ -12,7 +14,48 @@ const Template: Story<ContainerProps> = (args): React.ReactElement => (
   <Container {...args} />
 );
 
-export const Primary = Template.bind({});
-Primary.args = {
-  children: 'Hello World!',
+export const White1 = Template.bind({});
+White1.args = {
+  background: 'white1',
+  children: <Paragraph>{LOREM}</Paragraph>,
+};
+
+export const White2 = Template.bind({});
+White2.args = {
+  background: 'white2',
+  children: <Paragraph>{LOREM}</Paragraph>,
+};
+
+export const Primary2 = Template.bind({});
+Primary2.args = {
+  background: 'primary2',
+  children: <Paragraph>{LOREM}</Paragraph>,
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  background: 'primary2',
+  children: <Paragraph>{LOREM}</Paragraph>,
+  spacing: 'small',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  background: 'primary2',
+  children: <Paragraph>{LOREM}</Paragraph>,
+  spacing: 'medium',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  background: 'primary2',
+  children: <Paragraph>{LOREM}</Paragraph>,
+  spacing: 'large',
+};
+
+export const ExtraLarge = Template.bind({});
+ExtraLarge.args = {
+  background: 'primary2',
+  children: <Paragraph>{LOREM}</Paragraph>,
+  spacing: 'xlarge',
 };
