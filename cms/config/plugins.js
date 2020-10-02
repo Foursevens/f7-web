278a6f595b0cd90dad92ab8a4d91bdf2cb7aff7a
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-02 20:39:40 +0200

    feat(cms): add upload aws s3 provider

--- cms/config/plugins.js
+++ cms/config/plugins.js
@@ -1,8 +1,19 @@
 'use strict';
 
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     endpoint: '/graphql',
     shadowCRUD: true,
   },
-};
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: 'eu-west-1',
+      params: {
+        Bucket: env('AWS_UPLOAD_S3_BUCKET_NAME'),
+      },
+    },
+  },
+});
