b586bcd31fc5d4b9ea4a0c303b66e006c1539eec
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-05 12:02:19 +0200

    docs(design/title): add more title stories

--- design/components/title.stories.tsx
+++ design/components/title.stories.tsx
@@ -12,22 +12,31 @@ const Template: Story<TitleProps> = (args): React.ReactElement => (
   <Title {...args} />
 );
 
-export const size1 = Template.bind({});
-size1.args = {
+export const WithFullStop = Template.bind({});
+WithFullStop.args = {
   children: 'Eenvoudig fietsparkeren',
-  size: 'size1',
+  fullStop: true,
+};
+
+export const WithoutFullStop = Template.bind({});
+WithoutFullStop.args = {
+  children: 'Eenvoudig fietsparkeren',
   fullStop: false,
 };
 
-export const size2 = Template.bind({});
-size2.args = {
+export const WithSize1 = Template.bind({});
+WithSize1.args = {
+  children: 'Eenvoudig fietsparkeren',
+  size: 'size1',
+};
+
+export const WithSize2 = Template.bind({});
+WithSize2.args = {
   children: 'Digitaal advies',
   size: 'size2',
-  fullStop: false,
 };
-export const size3 = Template.bind({});
-size3.args = {
+export const WithSize3 = Template.bind({});
+WithSize3.args = {
   children: 'Digitaal voor mens en planeet',
   size: 'size3',
-  fullStop: false,
 };
