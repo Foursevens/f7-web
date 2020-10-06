46fbaf7270c56b565a3fe5eaa3815dfbb8ba3581
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-06 18:30:14 +0200

    chore(design): remove default size

--- design/components/title.tsx
+++ design/components/title.tsx
@@ -39,7 +39,7 @@ export function Title({
   as = 'div',
   children,
   fullStop = true,
-  size = 1,
+  size,
 }: TitleProps): React.ReactElement {
   return (
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
