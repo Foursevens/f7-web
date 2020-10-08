bd44d6b9c7314e15ffa514db1434883c9bd8f856
Naseer Kassar <naseer@foursevens.be>
2020-10-08 16:51:54 +0200

    fix(design/cards): fixing img size, more improvements on the card.stories.tsx file,  ,

--- design/components/card.stories.tsx
+++ design/components/card.stories.tsx
@@ -2,8 +2,7 @@ import { Story } from '@storybook/react/types-6-0';
 import React from 'react';
 
 import duurzaamheid from './assets/duurzaamheid.png';
-import { CardProps, Card, ContentContainer } from './card';
-import { RICH_TEXT, SERVICE } from './data';
+import { CardProps, Card } from './card';
 import { RichText } from './rich-text';
 import { Tag } from './tag';
 import { Title } from './title';
@@ -17,36 +16,25 @@ const Template: Story<CardProps> = (args): React.ReactElement => (
   <Card {...args} />
 );
 
-export const CaseWithImage = Template.bind({});
-CaseWithImage.args = {
-  href: '/',
+export const cardWithImageAndText = Template.bind({});
+cardWithImageAndText.args = {
+  img: duurzaamheid,
   children: [
-    <img alt="decorative" src={duurzaamheid} />,
-    <ContentContainer>
-      <main>
-        <Tag>Theme</Tag>
-        <Title as="h3" size={3}>
-          Eenvoudig fietsparkeren
-        </Title>
-        <RichText>{RICH_TEXT}</RichText>
-      </main>
-    </ContentContainer>,
+    <Tag>Theme</Tag>,
+    <Title size={3}>Eenvoudig fietsparkeren</Title>,
+    <RichText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua
+    </RichText>,
   ],
 };
 
-export const StoriesWithImage = Template.bind({});
-StoriesWithImage.args = {
-  href: '/',
+export const cardWithImage = Template.bind({});
+cardWithImage.args = {
+  img: duurzaamheid,
   children: [
-    <img alt="decorative" src={duurzaamheid} />,
-    <ContentContainer>
-      <main>
-        <Tag>Cyclo</Tag>
-        <Title as="h3" size={3}>
-          Eenvoudig fietsparkeren
-        </Title>
-        <RichText>{SERVICE}</RichText>
-      </main>
-    </ContentContainer>,
+    <Tag>Cyclo</Tag>,
+    <Title size={3}>Eenvoudig fietsparkeren</Title>,
+    <RichText>Research & Design</RichText>,
   ],
 };
--- design/components/card.tsx
+++ design/components/card.tsx
@@ -1,54 +1,59 @@
-import { useLink } from '@react-aria/link';
-import React, { useRef } from 'react';
+import React from 'react';
 
 import { styled } from './stitches.config';
 
-const StyledCard = styled('a', {
+const StyledCard = styled('section', {
   display: 'inline-block',
-  img: { height: '100%', width: '100%', transition: `transform .6s ease` },
+  img: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+    transition: `transform .6s ease`,
+  },
   outline: 'none',
   position: 'relative',
-  '.img-container': {
+
+  'a::after': { position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 },
+
+  '.card-img-container': {
     height: 324,
     width: 406,
     overflow: 'hidden',
   },
+
   ':first-child:hover img': {
     transform: 'scale(1.2)',
   },
+
+  '.CardContentContainer': {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    position: 'absolute',
+    top: 264,
+    left: 0,
+    width: 376,
+    backgroundColor: '#ffffff',
+    padding: 20,
+  },
+
+  '.CardContentContainer > *': {
+    marginBottom: 3,
+  },
 });
 
-export const ContentContainer = styled('div', {
-  display: 'flex',
-  position: 'absolute',
-  top: 264,
-  left: 0,
-  flexFlow: 'column nowrap',
-  width: 376,
-  backgroundColor: '#ffffff',
-  main: {
-    margin: '$medium',
-  },
-  'main > *': {
-    paddingBottom: 3,
-  },
-});
 export interface CardProps {
-  href: string;
-  target: string;
+  img: string;
   children: React.ReactNode;
 }
-export function Card({
-  children: [duurzaamheid, ...rest],
-  ...props
-}: CardProps): React.ReactElement {
-  const reference = useRef<HTMLElement>(null);
-  const { linkProps } = useLink(props as never, reference);
-  const { href } = props;
+
+export function Card({ img, children }: CardProps): React.ReactElement {
   return (
-    <StyledCard {...linkProps} href={href}>
-      <div className="img-container">{duurzaamheid}</div>
-      {rest}
+    <StyledCard>
+      <div className="card-img-container">
+        {' '}
+        <img alt="decorative" src={img} />,
+      </div>
+      <div className="CardContentContainer">{children}</div>
     </StyledCard>
   );
 }
