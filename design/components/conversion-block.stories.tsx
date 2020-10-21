67770388222ddef077883b9cef71ad8cdb16e6c8
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-21 14:05:28 +0200

    chore(design/conversion-block): rename things

--- design/components/conversion-block.stories.tsx
+++ design/components/conversion-block.stories.tsx
@@ -2,51 +2,48 @@ import { Story } from '@storybook/react/types-6-0';
 import React from 'react';
 
 import { ButtonLink } from './button-link';
-import { ConversationBlock, ConversionBlockProps } from './conversion-block';
+import { ConvertionBlock, ConversionBlockProps } from './conversion-block';
 import { RichText } from './rich-text';
 import { Title } from './title';
 
 export default {
-  title: '3. Organisms / ConversationBlock',
-  component: ConversationBlock,
+  title: '3. Organisms / ConversionBlock',
+  component: ConvertionBlock,
 };
 
 const Template: Story<ConversionBlockProps> = (args): React.ReactElement => (
-  <ConversationBlock {...args} />
+  <ConvertionBlock {...args} />
 );
 
-export const withOnlyTitle = Template.bind({});
-withOnlyTitle.args = {
+export const WithOnlyTitle = Template.bind({});
+WithOnlyTitle.args = {
   children: (
     <Title fullStop={false} size={2}>
       Klinkt als iets voor jou?
     </Title>
   ),
 };
-export const withOnlyButton = Template.bind({});
-withOnlyButton.args = {
+
+export const WithOnlyButton = Template.bind({});
+WithOnlyButton.args = {
   children: (
-    <div>
-      <ButtonLink background="secondary">Bekijk onze vacatures</ButtonLink>
-    </div>
+    <ButtonLink background="secondary">Bekijk onze vacatures</ButtonLink>
   ),
 };
 
-export const withOnlyText = Template.bind({});
-withOnlyText.args = {
+export const WithOnlyText = Template.bind({});
+WithOnlyText.args = {
   children: (
-    <div>
-      <RichText>
-        {
-          '<p><a href="mailto:info@foursevens.be">info@foursevens.be</a> <br> <a href="tel:+3234508030">+32 3 450 80 30</a></p>'
-        }
-      </RichText>
-    </div>
+    <RichText>
+      {
+        '<p><a href="mailto:info@foursevens.be">info@foursevens.be</a> <br> <a href="tel:+3234508030">+32 3 450 80 30</a></p>'
+      }
+    </RichText>
   ),
 };
 
-export const withText = Template.bind({});
-withText.args = {
+export const WithText = Template.bind({});
+WithText.args = {
   children: (
     <>
       <Title fullStop={false} size={2}>
@@ -64,8 +61,8 @@ withText.args = {
   ),
 };
 
-export const withNoText = Template.bind({});
-withNoText.args = {
+export const WithNoText = Template.bind({});
+WithNoText.args = {
   children: (
     <>
       <Title fullStop={false} size={2}>
