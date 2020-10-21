c952fc269cb55bd5b8e69b133e30c91497b87e6b
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-21 14:28:31 +0200

    chore(design/conversion-block): rename WithText to Primary

--- design/components/conversion-block.stories.tsx
+++ design/components/conversion-block.stories.tsx
@@ -15,6 +15,25 @@ const Template: Story<ConversionBlockProps> = (args): React.ReactElement => (
   <ConvertionBlock {...args} />
 );
 
+export const Primary = Template.bind({});
+Primary.args = {
+  children: (
+    <>
+      <Title fullStop={false} size={2}>
+        Klinkt als iets voor jou?
+      </Title>
+      <RichText size="lg">
+        {
+          '<p><a href="mailto:info@foursevens.be">info@foursevens.be</a> <br> <a href="tel:+3234508030">+32 3 450 80 30</a></p>'
+        }
+      </RichText>
+      <div>
+        <ButtonLink background="secondary">Bekijk onze vacatures</ButtonLink>
+      </div>
+    </>
+  ),
+};
+
 export const WithOnlyTitle = Template.bind({});
 WithOnlyTitle.args = {
   children: (
@@ -39,25 +58,6 @@ WithOnlyText.args = {
         '<p><a href="mailto:info@foursevens.be">info@foursevens.be</a> <br> <a href="tel:+3234508030">+32 3 450 80 30</a></p>'
       }
     </RichText>
-  ),
-};
-
-export const WithText = Template.bind({});
-WithText.args = {
-  children: (
-    <>
-      <Title fullStop={false} size={2}>
-        Klinkt als iets voor jou?
-      </Title>
-      <RichText>
-        {
-          '<p><a href="mailto:info@foursevens.be">info@foursevens.be</a> <br> <a href="tel:+3234508030">+32 3 450 80 30</a></p>'
-        }
-      </RichText>
-      <div>
-        <ButtonLink background="secondary">Bekijk onze vacatures</ButtonLink>
-      </div>
-    </>
   ),
 };
 
