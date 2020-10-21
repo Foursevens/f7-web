16c1835c0e5f3460e3be792ce6f6c446dbd5f96c
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-21 14:05:28 +0200

    chore(design/conversion-block): rename things

--- design/components/conversion-block.tsx
+++ design/components/conversion-block.tsx
@@ -3,15 +3,7 @@ import React from 'react';
 import FoursevensIcon from '../assets/foursevens-icon.svg';
 import { styled } from './stitches.config';
 
-const ContentArea = styled('div', {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  alignSelf: 'center',
-  height: 140,
-});
-
-const ContentAreraWrapper = styled('div', {
+const StyledConversionBlock = styled('div', {
   backgroundColor: '$primary2',
   overflow: 'hidden',
   position: 'relative',
@@ -25,17 +17,25 @@ const ContentAreraWrapper = styled('div', {
   },
 });
 
+const ContentArea = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  alignSelf: 'center',
+  height: 170,
+});
+
 export interface ConversionBlockProps {
   children: React.ReactNode;
 }
 
-export function ConversationBlock({
+export function ConvertionBlock({
   children,
 }: ConversionBlockProps): React.ReactElement {
   return (
-    <ContentAreraWrapper>
+    <StyledConversionBlock>
       <FoursevensIcon />
       <ContentArea>{children}</ContentArea>
-    </ContentAreraWrapper>
+    </StyledConversionBlock>
   );
 }
