cd7cd108dcf3f40a44efba9115a793dd0dfa00b6
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-27 18:18:09 +0100

    feat: add localization

--- site/next.config.js
+++ site/next.config.js
@@ -4,6 +4,10 @@ const withPlugins = require('next-compose-plugins');
 const withModules = require('next-transpile-modules');
 
 module.exports = withPlugins([withModules(['@f7-web/design'])], {
+  i18n: {
+    locales: ['en', 'fr', 'nl'],
+    defaultLocale: 'en',
+  },
   target: 'serverless',
   webpack(config) {
     config.module.rules.push({
