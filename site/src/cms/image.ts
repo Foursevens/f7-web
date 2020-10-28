5915fa8d4fd3c30cdda8e94bed8cf7ea0f712dc2
Dieter Luypaert <dieterluypaert@gmail.com>
2020-10-28 11:51:57 +0100

    refactor(site/content-block): determine image position in content block

--- site/src/cms/image.ts
+++ site/src/cms/image.ts
@@ -1,26 +1,21 @@
 import { apiBaseUrl } from '../api';
 
-const EVEN = 2;
-
 export interface CmsImageModel {
   alternativeText?: string;
   caption?: string;
+  width: number;
   height: number;
   url: string;
-  width: number;
 }
 
-export interface SiteImageModel extends CmsImageModel {
-  position?: 'start' | 'end';
-}
+export interface SiteImageModel extends CmsImageModel {}
 
 export function cmsImageToSiteModel(
   image: CmsImageModel,
   index = 0,
 ): SiteImageModel {
-  const position = index % EVEN === 0 ? 'start' : 'end';
   const url = image.url.startsWith('http')
     ? image.url
     : `${apiBaseUrl}${image.url}`;
-  return { ...image, position, url };
+  return { ...image, url };
 }
