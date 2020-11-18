48f87ac0883bc50a8563aafdb0bfffb065419657
Dieter Luypaert <dieterluypaert@gmail.com>
2020-11-18 12:43:16 +0100

    🐛 fix foursevens background logo
    Setting a width / height on an SVG element didn't work properly.
    
    CSS transform works perfect when using `transform-origin`.

--- design/components/hero1.tsx
+++ design/components/hero1.tsx
@@ -18,11 +18,10 @@ const StyledHero1 = styled(Container.Outer, {
     zIndex: -1,
     svg: {
       position: 'absolute',
-      width: 200,
-      height: 200,
       right: 0,
       top: '50%',
-      transform: 'translate(25%, -50%)',
+      transform: 'translate(12.5%, -50%) scale(0.75)',
+      transformOrigin: 'center right',
     },
   },
 
@@ -39,17 +38,23 @@ const StyledHero1 = styled(Container.Outer, {
     img: { width: '100%', height: '100%', objectFit: 'cover' },
   },
 
+  md: {
+    '.hero1__background-area': {
+      svg: { transform: 'translate(25%, -50%)' },
+    },
+  },
+
   lg: {
     gridTemplateRows: 'auto 100px',
     '.hero1__background-area': {
       gridArea: '1 / 1 / 2 / 8',
       svg: {
-        width: 250,
-        height: 250,
-        left: 0,
-        bottom: 0,
         top: 'initial',
+        right: 'initial',
+        bottom: 0,
+        left: 0,
         transform: 'translateX(-25%)',
+        transformOrigin: 'bottom left',
       },
     },
     '.hero1__children': { gridArea: '1 / 2 / 2 / 7' },
