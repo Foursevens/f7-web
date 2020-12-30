92ccc99a6e3701983b60be288b070c1f4867e6f2
Dieter Luypaert <dieterluypaert@gmail.com>
2020-12-30 16:23:08 +0100

    🐛 fix homepage localization

--- cms/api/homepage/services/homepage.js
+++ cms/api/homepage/services/homepage.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const marked = require('marked');
-
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {
-  async find(populate) {
-    const [homepage] = await strapi
-      .query('homepage')
-      .find({ _limit: 1 }, populate);
-
-    if (homepage.hero != null && homepage.hero.content != null) {
-      homepage.hero.content = { en: marked(homepage.hero.content.en) };
-    }
-
-    return homepage;
-  },
-};
+module.exports = {};
